d78ef5693dd112a7c8a1fdbc4b2c469494691a3d
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-01-17 21:52:56 +0300

    Update admin-middleware.ts

--- src/lib/admin-middleware.ts
+++ src/lib/admin-middleware.ts
@@ -19,6 +19,7 @@ export interface AdminPermissions {
   canAccessRandy: boolean
   canAccessPromocodes: boolean
   canAccessActivityLogs: boolean
+  canAccessGames: boolean
   isSuperAdmin: boolean
 }
 
